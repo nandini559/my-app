4d361f0df598c010dc9ab0f5808bc649d796d01b
Nandini Tarafdar <nandinitarafdar481@gmail.com>
2024-12-13 20:17:44 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+import './App.css';
+import Index from '.';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="home/" element={<Index />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
